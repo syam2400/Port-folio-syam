99921d5dd1f58f70cc88835715490ca6c2ac33f8
syam2400 <syam2400@gmail.com>
2024-06-11 14:25:42 +0530

    dfd

--- portFolio/build_files.sh
+++ portFolio/build_files.sh
@@ -7,4 +7,6 @@ source venv/bin/activate
 pip3 install --upgrade pip
 
 pip3 install -r requirements.txt 
+python3.9 manage.py makemigrations  
+python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
