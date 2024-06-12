140937382a169776a8908bc6a32bce02185f88cd
syam2400 <syam2400@gmail.com>
2024-06-12 20:32:51 +0530

    hhf

--- portFolio/build_files.sh
+++ portFolio/build_files.sh
@@ -5,7 +5,8 @@ source venv/bin/activate
 
 # Upgrade pip
 pip3 install --upgrade pip
-
+pip3 install psycopg2
+pip3 install psycopg2-binary
 pip3 install -r requirements.txt 
 python3.9 manage.py makemigrations  
 python3.9 manage.py migrate 
